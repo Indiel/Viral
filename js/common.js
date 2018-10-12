c6cd39b5ddd3c42b080fb6d617c11faaf78e2801
Indiel <kori.yanagi@gmail.com>
2018-10-12 19:11:12 +0300

    Мобильная версия

--- js/common.js
+++ js/common.js
@@ -6,34 +6,43 @@ function initPreloader() {
 
 $(document).ready(function(){
     initPreloader();
+    sliderRemove();
 });
 
-const slider = $(".slide");
-slider.slick({
-    dots: true,
-    infinite: false,
-    speed: 300,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    fade: true,
-    verticalSwiping: true
-});
-
-slider.on('wheel', (function(e) {
-    e.preventDefault();
-
-    if (e.originalEvent.deltaY < 0) {
-        $(this).slick('slickPrev');
-    } else {
-        $(this).slick('slickNext');
-    }
-}));
+   
 
 
 function initParallax() {
 	var scene = document.querySelector('#parallax-bg');
-	var parallaxInstance = new Parallax(scene, {
+	var parallaxInstance = new Parallax(scene, {    
 		relativeInput: true,
 	});
 }
 initParallax();
+
+function sliderRemove(){
+    if ($(window).width() > 768){
+        const slider = $(".slide");
+        slider.slick({
+            dots: true,
+            infinite: false,
+            speed: 300,
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            fade: true,
+            verticalSwiping: true
+        });
+
+        $(".slide").on('wheel', (function(e) {
+            e.preventDefault();
+        
+            if (e.originalEvent.deltaY < 0) {
+                $(this).slick('slickPrev');
+            } else {
+                $(this).slick('slickNext');
+            }
+        })); 
+    }
+}
+
+$(window).resize(sliderRemove);
